4df3e82c7de168649083f0a6e4c48e60d5a1c10d
Semhkaramn <158337999+Semhkaramn@users.noreply.github.com>
2025-12-10 02:21:57 +0300

    Update route.ts

--- src/app/api/user/wallet/route.ts
+++ src/app/api/user/wallet/route.ts
@@ -1,5 +1,5 @@
 import { NextRequest, NextResponse } from "next/server";
-import prisma from "@/lib/prisma";
+import { prisma } from "@/lib/prisma";
 
 // Cüzdan adresini getir
 export async function GET(request: NextRequest) {
