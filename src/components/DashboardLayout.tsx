d348b7250012152ad3aa06bc508ee46a5580be76
Semhkaramn <158337999+Semhkaramn@users.noreply.github.com>
2025-12-11 12:05:23 +0300

    Update DashboardLayout.tsx

--- src/components/DashboardLayout.tsx
+++ src/components/DashboardLayout.tsx
@@ -18,7 +18,7 @@ export default function DashboardLayout({ children, showSponsorBanner = false }:
       <Sidebar />
 
       <div className="transition-all duration-300">
-        <main className="min-h-screen p-4 md:p-6 lg:p-8 pt-20 lg:pt-24 lg:ml-64">
+        <main className="min-h-screen p-4 md:p-6 lg:p-8 pt-16 lg:pt-20 lg:ml-64">
           {children}
         </main>
       </div>
