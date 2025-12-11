a9d0a592316eec57088c34d49434a771586bffa1
Semhkaramn <158337999+Semhkaramn@users.noreply.github.com>
2025-12-11 07:40:50 +0300

    Update route.ts

--- src/app/api/admin/statistics/user/[userId]/route.ts
+++ src/app/api/admin/statistics/user/[userId]/route.ts
@@ -14,21 +14,6 @@ export async function GET(
       where: { id: userId },
       include: {
         rank: true,
-        referredBy: {
-          select: {
-            id: true,
-            username: true,
-            firstName: true
-          }
-        },
-        referrals: {
-          select: {
-            id: true,
-            username: true,
-            firstName: true,
-            createdAt: true
-          }
-        }
       }
     })
 
