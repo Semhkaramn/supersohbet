db6c3d4ad4f6405778288ac571b7a51f34acf540
Semhkaramn <158337999+Semhkaramn@users.noreply.github.com>
2025-12-11 10:20:15 +0300

    Update route.ts

--- src/app/api/admin/sponsors/route.ts
+++ src/app/api/admin/sponsors/route.ts
@@ -14,6 +14,7 @@ export async function GET(request: Request) {
             select: {
               id: true,
               telegramId: true,
+              siteUsername: true,
               username: true,
               firstName: true,
               lastName: true,
