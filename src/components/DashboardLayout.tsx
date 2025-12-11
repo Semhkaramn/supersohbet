7672e390c6ecc460e528457fd299334b976fc250
Semhkaramn <158337999+Semhkaramn@users.noreply.github.com>
2025-12-11 06:56:12 +0300

    Update DashboardLayout.tsx

--- src/components/DashboardLayout.tsx
+++ src/components/DashboardLayout.tsx
@@ -11,12 +11,11 @@ interface DashboardLayoutProps {
 export default function DashboardLayout({ children }: DashboardLayoutProps) {
   return (
     <div className="min-h-screen bg-slate-950">
+      <Header />
       <Sidebar />
 
       <div className="lg:pl-20 transition-all duration-300">
-        <Header />
-
-        <main className="min-h-screen p-4 md:p-6 lg:p-8">
+        <main className="min-h-screen p-4 md:p-6 lg:p-8 lg:pt-24">
           {children}
         </main>
       </div>
